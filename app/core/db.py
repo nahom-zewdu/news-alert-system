e0488ccbd1644f4dbb76e44567df9cbf0e7258a6
Nahom <nahiyo86@gmail.com>
2025-12-19 09:15:16 +0300

    chore: add logging

--- app/core/db.py
+++ app/core/db.py
@@ -3,8 +3,11 @@
 from mongoengine import connect
 import os
 from app.core.config import settings
+import logging
 
+logger = logging.getLogger(__name__)
 
 def init_db():
+    logger.info("Connecting to MongoDB")
     MONGO_URI = settings.MONGO_URI
     connect(host=MONGO_URI)
